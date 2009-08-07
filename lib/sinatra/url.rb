924102edf32bcc0e0bfb0684ec0aae79ca15e29f
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-08-07 18:58:07 -0300

    Make url_for support parametrized urls

--- lib/sinatra/url.rb
+++ lib/sinatra/url.rb
@@ -19,22 +19,73 @@ module Sinatra
       end
 
       def []=(name, path)
-        @map[name] = lambda do |*args|
-          path
+        @map[name] = Route.new(path)
+      end
+    end
+
+    class Route
+      def initialize(path)
+        @path = path
+        @pattern, @keys = compile(path)
+      end
+
+      def match(str)
+        @pattern.match(str)
+      end
+
+      def keys
+        @keys
+      end
+
+      def call(params)
+        params[:splat] = Array(params[:splat])
+        url = @path.dup
+        keys.each do |key|
+          match = key == "splat" ? "*" : ":#{key}"
+          replacement = key == "splat" ? params[:splat].shift : params[key.to_sym]
+          url.sub! match, replacement
         end
-        path
+        url
+      end
+
+      private
+
+      def compile(path)
+        keys = []
+        if path.respond_to? :to_str
+          special_chars = %w{. + ( )}
+          pattern =
+            path.to_str.gsub(/((:\w+)|[\*#{special_chars.join}])/) do |match|
+              case match
+              when "*"
+                keys << 'splat'
+                "(.*?)"
+              when *special_chars
+                Regexp.escape(match)
+              else
+                keys << $2[1..-1]
+                "([^/?&#]+)"
+              end
+            end
+          [/^#{pattern}$/, keys]
+        elsif path.respond_to? :match
+          [path, keys]
+        else
+          raise TypeError, path
+        end
       end
     end
 
     module DSL
       def url(name, path)
         url_map[name] = path
+        path
       end
     end
 
     module Helpers
-      def url_for(name, *args)
-        self.class.url_map[name].call(*args)
+      def url_for(name, params={})
+        self.class.url_map[name].call(params)
       end
     end
   end
--- test/test_url.rb
+++ test/test_url.rb
@@ -8,7 +8,6 @@ class TestUrl < Test::Unit::TestCase
           assert_equal "/foo", url_for(:foo)
         end
       }
-
       get "/foo"
       assert last_response.ok?
     end
@@ -19,8 +18,29 @@ class TestUrl < Test::Unit::TestCase
           assert_equal "bar", name
         end
       }
-
       get "/foo/bar"
+      assert last_response.ok?
+    end
+  end
+
+  context "generating urls with url_for for registered handlers" do
+    test "generating urls with :named_params" do
+      mock_app {
+        get url(:foo, "/foo/:name") do |name|
+          assert_equal "/foo/bar", url_for(:foo, :name => "bar")
+        end
+      }
+      get "/foo/bar"
+      assert last_response.ok?
+    end
+
+    test "generating urls with splat args" do
+      mock_app {
+        get url(:foo, "/say/*/to/*") do
+          assert_equal "/say/hi/to/mom", url_for(:foo, :splat => ["hi", "mom"])
+        end
+      }
+      get "/say/hi/to/mom"
       assert last_response.ok?
     end
   end
